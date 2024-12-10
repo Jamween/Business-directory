4807f41cf4a25bfec7d2b95611a6b087bf07b564
jamween <jamielukeconcannon@gmail.com>
2024-12-10 23:20:52 +0000

    read.js updated to fetch data

--- App.js
+++ App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+// client/src/App.js
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import NavigationBar from './components/NavigationBar';
+
+import Read from './components/Read';
+import Create from './components/Create';
+import Edit from './components/Edit';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <NavigationBar />
+      <Routes>
+        <Route path="/" element={<Read />} />
+        <Route path="/read" element={<Read />} />
+        <Route path="/create" element={<Create />} />
+        <Route path='/edit/:id' element={<Edit />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ components/Read.js
@@ -0,0 +1,30 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+import Businesses from "./Businesses"; // handle displaying individual business items.
+
+function Read() {
+  const [businesses, setBusinesses] = useState([]); // store fetched data
+
+  const fetchData = () => {
+    axios.get('http://localhost:4000/api/businesses')
+      .then((response) => {
+        setBusinesses(response.data); // Update state with fetched data
+      })
+      .catch((error) => {
+        console.error("Error fetching businesses:", error);
+      });
+  };
+
+  useEffect(() => {
+    fetchData(); // Fetch data 
+  }, []);
+
+  return (
+    <div>
+      <h2>Business List</h2>
+      <Businesses myBusinesses={businesses} ReloadData={fetchData} />
+    </div>
+  );
+}
+
+export default Read;
